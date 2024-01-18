608e343b1db43e3ef91ad4094a3097216bc91413
Laura Chege <lauracahege9@gmail.com>
2024-01-18 14:42:13 +0300

    handling error function and catch error function

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener("click", function(event) {
 
     fetch("<https://official-joke-api.appspot.com/random_joke>")   //fetch the api to request the joke endpoint
     .then((response) => response.json())
-    .then((data) => renderJoke(data));
+    .then((data) => renderJoke(data))
+    .catch(() => renderError()); 
 });
 
 function renderJoke(data) {                                      //render the joke and pass it to the new function (html)
@@ -14,4 +15,7 @@ function renderJoke(data) {                                      //render the jo
     punchline.innerHTML = data.punchline;
 }
 
-
+function renderError() {                                       //render the error message if the api is not working to add a message to HTML element
+    const error = document.getElementById("error");
+    error.innerHTML = "Error: Something went wrong. Please try again later!";
+}
