0d25e811452d5efba3fdc0b542f5ee7d8838ef2e
Laura Chege <laurachege9@gmail.com>
2024-01-18 14:25:38 +0300

    fetch data from API using js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.addEventListener("click", function(event) {
     //check if button was clicked
     if (!event.target.matches("#button")) return;
 
-    console.log("Button was clicked!");
+    fetch("<https://official-joke-api.appspot.com/random_joke>")   //fetch the api to request the joke endpoint
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 });
 
 
